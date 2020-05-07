24f1d9b757477a0ae259fd3f4667241c49d99e82
preetiwali <preetiwali212@gmail.com>
2020-05-07 18:01:00 +0400

    Updates

---
+++ precache-manifest.8d4781c004bb756c73f07b4ee1ad1a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e54c221f77b487d877811d3ccaf4b3",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "34470fcaf2537508dbf8",
+    "url": "/todoList/static/css/main.b390e503.chunk.css"
+  },
+  {
+    "revision": "1b37d526b3f038ce77b3",
+    "url": "/todoList/static/js/2.0af8fa4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList/static/js/2.0af8fa4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34470fcaf2537508dbf8",
+    "url": "/todoList/static/js/main.014d5e3d.chunk.js"
+  },
+  {
+    "revision": "8c56d5a3713e55de3259",
+    "url": "/todoList/static/js/runtime-main.155002a9.js"
+  }
+]);
